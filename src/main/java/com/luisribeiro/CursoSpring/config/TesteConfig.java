2242d6d6353f73c0270b95803c8583746334e5b7
luisdosreisribeiro <luisalberto677@gmail.com>
2021-06-24 21:59:13 -0300

    OrderItem, many-to-many association with extra attributes PART2

--- src/main/java/com/luisribeiro/CursoSpring/config/TesteConfig.java
+++ src/main/java/com/luisribeiro/CursoSpring/config/TesteConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.luisribeiro.CursoSpring.entities.Category;
 import com.luisribeiro.CursoSpring.entities.Order;
+import com.luisribeiro.CursoSpring.entities.OrderItem;
 import com.luisribeiro.CursoSpring.entities.Product;
 import com.luisribeiro.CursoSpring.entities.User;
 import com.luisribeiro.CursoSpring.entities.enums.OrderStatus;
 import com.luisribeiro.CursoSpring.repositories.CategoryRepository;
+import com.luisribeiro.CursoSpring.repositories.OrderItemRepository;
 import com.luisribeiro.CursoSpring.repositories.OrderRepository;
 import com.luisribeiro.CursoSpring.repositories.ProductRepository;
 import com.luisribeiro.CursoSpring.repositories.UserRepository;
@@ -21,6 +23,9 @@ import com.luisribeiro.CursoSpring.repositories.UserRepository;
 @Configuration
 @Profile("test")
 public class TesteConfig implements CommandLineRunner{
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Autowired
 	private OrderRepository orderRepository;
@@ -74,6 +79,13 @@ public class TesteConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4 ));
 		
 	}
 
