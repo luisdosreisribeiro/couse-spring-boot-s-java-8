0fe8bbdfe8ee8df1203ef37c34309ef0707568e7
luisdosreisribeiro <luisalberto677@gmail.com>
2021-06-25 18:47:26 -0300

    Payment, one-to-one association

--- src/main/java/com/luisribeiro/CursoSpring/entities/Order.java
+++ src/main/java/com/luisribeiro/CursoSpring/entities/Order.java
@@ -43,6 +43,7 @@ public class Order implements Serializable {
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
+	
 	public Order() {
 
 	}
@@ -102,6 +103,14 @@ public class Order implements Serializable {
 		return items;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x: items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
