a496216f285be65c9f7e332260643862f55666f7
luisdosreisribeiro <luisalberto677@gmail.com>
2021-06-26 21:19:59 -0300

    Exception handling - findById

--- src/main/java/com/luisribeiro/CursoSpring/service/UserService.java
+++ src/main/java/com/luisribeiro/CursoSpring/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import com.luisribeiro.CursoSpring.entities.User;
 import com.luisribeiro.CursoSpring.repositories.UserRepository;
+import com.luisribeiro.CursoSpring.service.exceptions.ResourceNotFoundException;
 
 @Component
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 		
 	}
 	
