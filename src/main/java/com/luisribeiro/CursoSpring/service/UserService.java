2fb139491cb193c0b23d87677dfc5469e56ef811
luisdosreisribeiro <luisalberto677@gmail.com>
2021-06-27 20:27:56 -0300

    Exception handling - update

--- src/main/java/com/luisribeiro/CursoSpring/service/UserService.java
+++ src/main/java/com/luisribeiro/CursoSpring/service/UserService.java
@@ -3,6 +3,8 @@ package com.luisribeiro.CursoSpring.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -15,41 +17,48 @@ import com.luisribeiro.CursoSpring.service.exceptions.ResourceNotFoundException;
 
 @Component
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
-		
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+
 	}
-	
+
 	public void delete(Long id) {
 		try {
-		repository.deleteById(id);
-		}catch(EmptyResultDataAccessException e) {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseExceptinons(e.getMessage());
 		}
-		
+
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity =  repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
-private void updateData(User entity, User obj) {
+
+	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 
 //
@@ -57,7 +66,7 @@ private void updateData(User entity, User obj) {
 //		repository.save(usuario);
 //		
 //	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
